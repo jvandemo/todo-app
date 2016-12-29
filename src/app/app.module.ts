7d6d1e2dded37fd86d86c49f97896cac778189f5
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-12-29 16:49:35 +0100

    feat(todo-list-footer): add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoListFooterComponent
   ],
   imports: [
     BrowserModule,
