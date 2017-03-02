0820bb81fbdab84230147a3c638233ee4aaed17c
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2017-03-02 08:15:55 +0100

    feat(TodoListItemComponent): change attribute selector to element selector

--- src/app/todo-list-item/todo-list-item.component.ts
+++ src/app/todo-list-item/todo-list-item.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from '../todo';
 
 @Component({
-  selector: '[app-todo-list-item]',
+  selector: 'app-todo-list-item',
   templateUrl: './todo-list-item.component.html',
   styleUrls: ['./todo-list-item.component.css']
 })
